700019cfc0efcd8c494bfc9e29e8d26db94a7713
Iskander <alimov.ikh@phystech.edu>
2023-06-14 15:50:35 +0300

    add events model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Events(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='events_image', null=True)
+    description_mini = models.TextField(null=True)
+    description_full = models.TextField(null=True)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    active = models.BooleanField(default=1)
--- task1/urls.py
+++ task1/urls.py
@@ -22,9 +22,9 @@ from main_page.views import index, contacts, contact_with_us, olympic_hse, redir
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('events', include('events.urls', namespace='events')),
-    path('contacts', contacts, name='contacts'),
-    path('contact_with_us', contact_with_us, name='contact_with_us'),
-    path('olympic_hse', olympic_hse, name='olympic_hse'),
-    path('redirect_page', redirect_page, name='redirect_page')
+    path('events/', include('events.urls', namespace='events')),
+    path('contacts/', contacts, name='contacts'),
+    path('contact_with_us/', contact_with_us, name='contact_with_us'),
+    path('olympic_hse/', olympic_hse, name='olympic_hse'),
+    path('redirect_page/', redirect_page, name='redirect_page')
 ]
